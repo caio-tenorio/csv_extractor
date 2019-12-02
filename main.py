53a9bc61bbf73f9d30a13d4e212dca55e5354cca
Caio Cavalcanti <ccat@cin.ufpe.br>
2019-12-02 01:17:02 -0300

    Refactor

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import database_utils
 
 
 def main():
-    # extract_csv()
-    # spreadsheet_extractor()
-    # database_utils.put_key_to_verbas()
-    # database_utils.put_key_to_materias()
-    # database_utils.populate_presencas()
-    # database_utils.put_key_to_remuneracoes()
+    extract_csv()
+    spreadsheet_extractor()
+    database_utils.put_key_to_verbas()
+    database_utils.put_key_to_materias()
+    database_utils.populate_presencas()
+    database_utils.put_key_to_remuneracoes()
     database_utils.fix_date_format_of_sessoes()
+
 
 if __name__ == '__main__':
     main()
